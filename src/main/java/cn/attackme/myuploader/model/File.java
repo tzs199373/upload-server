e248d1ad630fdc15f16dccca249701f398676af3
����ɽ <544610616@qq.com>
2020-04-05 10:52:33 +0800

    初始

---
+++ src/main/java/cn/attackme/myuploader/model/File.java
@@ -0,0 +1,79 @@
+package cn.attackme.myuploader.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * File表存储上传的文件信息
+ */
+@Data
+@AllArgsConstructor
+@ToString
+public class File implements Serializable {
+
+    private static final long serialVersionUID = -6956947981866795431L;
+
+    private Long id;
+    private String name;
+    private String md5;
+    private String path;
+    private Date uploadTime;
+
+    public File() {
+    }
+
+    public File(String name, String md5, String path, Date uploadTime) {
+        this.name = name;
+        this.md5 = md5;
+        this.path = path;
+        this.uploadTime = uploadTime;
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMd5() {
+        return md5;
+    }
+
+    public void setMd5(String md5) {
+        this.md5 = md5;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public Date getUploadTime() {
+        return uploadTime;
+    }
+
+    public void setUploadTime(Date uploadTime) {
+        this.uploadTime = uploadTime;
+    }
+}
